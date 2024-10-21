319d07fe6a5babd903e998842abcd765f4eab0d8
py1u.dev <py1u.dev@gmail.com>
2024-10-20 23:02:44 -0700

    update: hero gradient styling

--- src/components/home/Root.jsx
+++ src/components/home/Root.jsx
@@ -1,22 +1,29 @@
 import { poppins } from "@/utils/fonts";
 import Image from "next/image";
 import sorafiLogo from "@/public/logo.svg";
+import ToastFail from "../toast/ToastFail";
+import ToastNotify from "../toast/ToastNotify";
+import ToastSuccess from "../toast/ToastSuccess";
+import ToastWarn from "../toast/ToastWarn";
 const Home = () => {
   return (
     <div className="flex flex-col items-center justify-center font-light">
-      <div className="my-10 flex w-9/12 flex-col items-center justify-center rounded-3xl bg-gradient-to-r from-sorafi-stormGrey to-sorafi-cherrySakura p-48 text-center">
-        <p className="text-6xl text-white">
+      <div className="my-10 flex flex-col items-center justify-center rounded-3xl bg-gradient-to-r from-sorafi-sakura-300 to-sorafi-sky-500 p-48 text-center">
+        <p className="text-5xl text-white">
           Revolutionizing How You Manage Digital Assets
         </p>
       </div>
       <Image src={sorafiLogo} alt="sorafi logo" height={120} width={120} />
       <div className={`${poppins.className} my-10 w-3/5 text-center text-xl`}>
-        <h2 className="text-center text-4xl">Introducing Sorafi</h2>
-        <p className="mx-auto mt-10 lg:w-1/4">
-          The decentralized wallet that lets you securely manage crypto, fiat,
-          and digital assets—with confidence.
+        <h2 className="text-center text-4xl font-medium">Introducing Sorafi</h2>
+        <p className="mx-auto mt-10 xl:w-1/2">
+          The decentralized wallet that lets you securely manage crypto and
+          digital assets with confidence.
         </p>
-        <p></p>
+        <ToastFail />
+        <ToastNotify />
+        <ToastSuccess />
+        <ToastWarn />
       </div>
     </div>
   );
--- src/style/template.js
+++ src/style/template.js
@@ -5,7 +5,7 @@ export const sorafiDefaultLight =
   "flex min-h-screen flex-col place-items-center justify-center bg-sorafi-light font-oxanium  text-black";
 
 export const sakuraGradient =
-  "bg-gradient-to-r from-sorafi-sky-400 to-sorafi-sakura-300 bg-clip-text text-transparent";
+  "bg-gradient-to-r from-sorafi-sky-500 to-sorafi-sakura-500 bg-clip-text text-transparent";
 
 export const sorafiTitle =
   "flex items-center text-2xl font-medium font-oxanium bg-gradient-to-r from-sorafi-stormGrey to-sorafi-cherrySakura bg-clip-text text-transparent";
